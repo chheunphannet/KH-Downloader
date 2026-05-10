0c2ad1de5046bf2d20535f3ee31475f79c858b33
Chheun Phanet <chhernphannet0001@gmail.com>
2026-05-10 15:22:01 +0700

    fix withMiddlware in app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,14 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin.api' => EnsureAdminApiToken::class,
         ]);
+        $middleware->preventRequestForgery(except: [
+            'sitemap.xml',
+            'robots.txt',
+        ]);
+        $middleware->encryptCookies(except: [
+            'sitemap.xml',
+            'robots.txt',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
